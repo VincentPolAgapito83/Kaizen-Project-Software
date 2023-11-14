0417f82177dfae2052aca3d40493cf410ba55626
VincentPolAgapito83 <agentonce83@gmail.com>
2023-11-14 09:16:49 +0800

    Sixth Commit

--- Kaizen/Kaizen/articles/urls.py
+++ Kaizen/Kaizen/articles/urls.py
@@ -4,5 +4,6 @@ from .import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-    path('', views.article_list, name="list"),
+    path('', views.article_list,name="list"),
+    path('{?P<slug>[\w-]+}', views.article_details, name="detail")
 ]
--- Kaizen/Kaizen/articles/views.py
+++ Kaizen/Kaizen/articles/views.py
@@ -6,5 +6,6 @@ def article_list(request):
     articles = Article.objects.all().order_by('date')
     return render(request, 'articles/article_list.html', {'articles': articles})  
 
-
+def article_details(request, slug):  
+    return HttpResponse(slug)
 
